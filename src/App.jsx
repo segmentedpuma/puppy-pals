51a8952daaed30a5c2c72e16c9fe6f1fc31989f0
Gabriel <segmentedpuma@LAPTOP-DKL1MBK8>
2024-04-04 13:27:26 -0500

    i forgor to turn this in

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log(puppies);
+  function handleClick() {
+    console.log('clicked');
+  }
 
   return (
-    <>
+    <div className="App">
       
-    </>
+        {puppies.map((puppy) => {
+            return (
+            <p onClick={handleClick(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </p>
+            )
+        })}
+     
+      < p >{featPupId}</p>
+    </div>
   )
 }
 
